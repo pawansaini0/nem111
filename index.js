e802d8421843ef83dba1575262e64aef5e28ab86
unknown <sainipawann0@gmail.com>
2023-06-12 17:13:34 +0530

    done

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
+app.get("/", (req, res)=>{
+res.send("<h1>Welcome</h1>")
+})
+
 app.use("/users", userRouter);
 app.use("/posts", instaRouter);
 
